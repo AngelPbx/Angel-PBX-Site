49ff40b8f439600f88874e19abe165b0ba04f736
webviosystem10 <riddhee25@gmail.com>
2024-11-26 12:35:04 +0530

    12

--- pages/_app.js
+++ pages/_app.js
@@ -9,23 +9,24 @@ import store from "../components/store";
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
-    <>
-      <Head>
-      <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    {/* <!-- BootStrap Icons --> */}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
-    {/* <!-- Font Awesome Icons --> */}
-    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.5.2/css/all.css"/>
-    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.5.2/css/sharp-thin.css"/>
-    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.5.2/css/sharp-solid.css"/>
-    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.5.2/css/sharp-regular.css"/>
-    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.5.2/css/sharp-light.css"></link>
-      </Head>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </>
+      <>
+        <Head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          {/* <!-- BootStrap Icons --> */}
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+          {/* <!-- Font Awesome Icons --> */}
+          <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.5.2/css/all.css" />
+          <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.5.2/css/sharp-thin.css" />
+          <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.5.2/css/sharp-solid.css" />
+          <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.5.2/css/sharp-regular.css" />
+          <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.5.2/css/sharp-light.css"></link>
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        </Head>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </>
     </Provider>
   );
 }
